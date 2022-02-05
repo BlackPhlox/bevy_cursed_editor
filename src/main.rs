40af3456ee14461899893a8b0ec9624c2d2b0835
Mikkel Luja Rasmussen <milr@itu.dk>
2022-02-05 13:13:44 +0100

    Using custom codegen

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
 extern crate codegen;
-use codegen::Scope;
+use codegen::{Function, Scope};
+use serde::{Serialize, Deserialize};
 use std::{
-    fmt::Debug,
     fs::{self, File},
     io::Write,
     path::Path,
     process::Command,
 };
 
+#[derive(Serialize, Deserialize)]
 struct BevyModel {
+    #[serde(skip)]
     scope: Scope,
+}
+
+trait BevyCodegen {
+    fn create_app(&mut self, content: &str) -> &mut Function;
+
+    fn create_query(&mut self, content: &str) -> &mut Function;
+}
+impl BevyCodegen for Scope {
+    fn create_app(&mut self, content: &str) -> &mut Function {
+        self.raw("#[bevy_main]")
+            .new_fn("main")
+            .line(format!("App::new(){}.run();", content))
+    }
+
+    fn create_query(&mut self, content: &str) -> &mut Function {
+        self.new_fn(format!("{}_system", content).as_str())
+        .line(format!("println!(\"{}\");", content))
+    }
 }
 
 fn main() {
@@ -19,46 +39,29 @@ fn main() {
 
     bevy_model.scope.import("bevy::prelude", "*");
 
+    let hw = "hello_world";
+    let hws = format!(".add_system({}_system)", hw);
     bevy_model
         .scope
-        .new_struct("Foo")
-        .derive("Debug")
-        .field("one", "usize")
-        .field("two", "String");
-
-    bevy_model.scope.raw("#[bevy_main]");
-    bevy_model
-        .scope
-        .new_fn("main")
-        .line("App::new().add_system(hello_world_system).run();");
+        .create_app(&hws);
 
     bevy_model
         .scope
-        .new_fn("hello_world_system")
-        .line("println!(\"hello world\");");
+        .create_query(&hw);
 
     println!("{}", bevy_model.scope.to_string());
 
+    let serialized = serde_json::to_string(&bevy_model).unwrap();
+    println!("serialized = {}", serialized);
+
     let _ = write_to_file(bevy_model);
 
-    //Action 1
-    let output = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args(["/C", "echo hello"])
-            .output()
-            .expect("failed to execute process")
-    } else {
-        Command::new("sh")
-            .arg("-c")
-            .arg("echo hello")
-            .output()
-            .expect("failed to execute process")
-    };
-    let stdout = String::from_utf8(output.stdout).unwrap();
-    println!("{:?}", stdout);
+    build_and_run();
+}
 
+fn build_and_run() {
     println!("fmt");
-    let fmt = Command::new("cargo")
+    let _fmt = Command::new("cargo")
         .arg("fmt")
         .arg("--all")
         .current_dir("bevy")
@@ -66,7 +69,7 @@ fn main() {
         .expect("failed to execute cargo fmt");
 
     println!("fix");
-    let fix = Command::new("cargo")
+    let _fix = Command::new("cargo")
         .arg("clippy")
         .arg("--fix")
         .arg("--allow-no-vcs")
@@ -75,7 +78,7 @@ fn main() {
         .expect("failed to execute cargo fix");
 
     println!("clippy");
-    let clippy = Command::new("cargo")
+    let _clippy = Command::new("cargo")
         .arg("clippy")
         .arg("--")
         .arg("-D")
@@ -84,10 +87,10 @@ fn main() {
         .status() //output()
         .expect("failed to execute cargo clippy");
 
-    println!("{:?}", clippy);
+    //println!("{:?}", clippy);
 
     println!("run");
-    let run = Command::new("cargo")
+    let _run = Command::new("cargo")
         .arg("run")
         .current_dir("bevy")
         .status() //output()
@@ -99,7 +102,7 @@ fn write_to_file(model: BevyModel) -> std::io::Result<()> {
     const SRC_FOLDER: &str = "src";
     if Path::new(BEVY_FOLDER).exists() {
         fs::remove_dir_all(BEVY_FOLDER.to_owned() + "/" + &SRC_FOLDER.to_owned())?;
-        fs::remove_file(BEVY_FOLDER.to_owned() + "/" + "Cargo.toml");
+        let _rf = fs::remove_file(BEVY_FOLDER.to_owned() + "/" + "Cargo.toml");
     } else {
         fs::create_dir(BEVY_FOLDER)?;
     }
