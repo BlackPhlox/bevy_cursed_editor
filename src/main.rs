a10fb5d0010ca6e853d2d5c2a40133e31a5a4b8b
Mikkel Luja Rasmussen <milr@itu.dk>
2022-02-08 00:13:46 +0100

    Added plugins and examples

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::{
     process::Command,
 };
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Default)]
 struct BevyModel {
     plugins: Vec<Plugin>,
     components: Vec<Component>,
@@ -16,10 +16,11 @@ struct BevyModel {
     systems: Vec<System>,
     bevy_settings: Settings,
     model_meta: Meta,
+    examples: Vec<BevyModel>,
 }
 
 impl BevyModel {
-    fn generate(&mut self) -> Scope {
+    fn generate(&self) -> Scope {
         let mut scope = Scope::new();
 
         scope.import("bevy::prelude", "*");
@@ -48,7 +49,12 @@ impl BevyModel {
         app_code_merge.push_str(&plugin_app_code);
         app_code_merge.push_str(&startup_system_app_code);
         app_code_merge.push_str(&system_app_code);
-        scope.create_app(&app_code_merge);
+
+        match &self.model_meta.bevy_type {
+            BevyType::Plugin(s) => scope.create_plugin(s, false, &app_code_merge),
+            BevyType::PluginGroup(s) => scope.create_plugin(s, true, &app_code_merge),
+            _ => scope.create_app(&app_code_merge),
+        };
 
         for component in &self.components {
             scope.create_component(&component.name);
@@ -64,9 +70,26 @@ impl BevyModel {
     }
 }
 
+#[derive(PartialEq, Eq, Serialize, Deserialize, Clone)]
+enum BevyType {
+    App,
+    Plugin(String),
+    PluginGroup(String),
+    Example,
+}
 #[derive(Serialize, Deserialize, Clone)]
 struct Meta {
     name: String,
+    bevy_type: BevyType,
+}
+
+impl Default for Meta {
+    fn default() -> Self {
+        Self {
+            name: "bevy_default_meta".to_string(),
+            bevy_type: BevyType::App,
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -86,9 +109,10 @@ struct Plugin {
     is_group: bool,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Default)]
 struct Settings {
     features: Vec<Feature>,
+    dev_features: Vec<Feature>,
 }
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -175,6 +199,8 @@ impl Feature {
 trait BevyCodegen {
     fn create_app(&mut self, content: &str) -> &mut Function;
 
+    fn create_plugin(&mut self, name: &str, is_group: bool, content: &str) -> &mut Function;
+
     fn create_query(&mut self, name: &str, content: &str) -> &mut Function;
 
     fn create_component(&mut self, name: &str) -> &mut Struct;
@@ -184,6 +210,21 @@ impl BevyCodegen for Scope {
         self.raw("#[bevy_main]")
             .new_fn("main")
             .line(format!("App::new(){}.run();", content))
+    }
+
+    fn create_plugin(&mut self, name: &str, is_group: bool, content: &str) -> &mut Function {
+        self.new_struct(name).vis("pub");
+        let plugin_impl = match is_group {
+            false => self.new_impl(name).impl_trait("Plugin"),
+            true => self.new_impl(name).impl_trait("Plugins"),
+        };
+        plugin_impl
+            .new_fn("build")
+            .arg_ref_self()
+            .arg("app", "&mut App")
+            .line("app")
+            .line(content)
+            .line(";")
     }
 
     fn create_query(&mut self, name: &str, content: &str) -> &mut Function {
@@ -199,12 +240,16 @@ fn main() {
     let mut bevy_model = BevyModel {
         model_meta: Meta {
             name: "bevy_test".to_string(),
+            bevy_type: BevyType::Plugin("BevyTest".to_string()),
         },
-        plugins: vec![],
-        components: vec![],
-        startup_systems: vec![],
-        systems: vec![],
-        bevy_settings: Settings { features: vec![] },
+        examples: vec![BevyModel {
+            model_meta: Meta {
+                name: "example_test".to_string(),
+                bevy_type: BevyType::Example,
+            },
+            ..Default::default()
+        }],
+        ..Default::default()
     };
 
     bevy_model.bevy_settings.features.push(Feature::Render);
@@ -279,12 +324,25 @@ fn build_and_run(model: BevyModel) {
         .status() //output()
         .expect("failed to execute cargo clippy");
 
-    println!("run");
-    let _run = Command::new("cargo")
-        .arg("run")
-        .current_dir(path)
-        .status() //output()
-        .expect("failed to execute cargo run");
+    if let BevyType::App = model.model_meta.bevy_type {
+        println!("run");
+        let _run = Command::new("cargo")
+            .arg("run")
+            .current_dir(path.clone())
+            .status() //output()
+            .expect("failed to execute cargo run");
+    }
+
+    println!("example(s)");
+    for example in model.examples {
+        let _run = Command::new("cargo")
+            .arg("run")
+            .arg("--example")
+            .arg(example.model_meta.name)
+            .current_dir(path.clone())
+            .status() //output()
+            .expect("failed to execute cargo run");
+    }
 
     /*
     Open generated project in VSCode
@@ -297,7 +355,25 @@ fn build_and_run(model: BevyModel) {
     */
 }
 
-fn write_to_file(mut model: BevyModel) -> std::io::Result<()> {
+fn feature_write(features: Vec<Feature>) -> String {
+    let mut features_str = "".to_owned();
+    if features.is_empty() {
+        features_str.push_str("default-features = false");
+    } else {
+        features_str += "features = [";
+        let len = features.len();
+        for (i, feature) in features.into_iter().enumerate() {
+            features_str += format!("\"{}\"", feature.to_feature()).as_str();
+            if i != len - 1 {
+                features_str += ",";
+            }
+        }
+        features_str += "]";
+    }
+    features_str
+}
+
+fn write_to_file(model: BevyModel) -> std::io::Result<()> {
     let bevy_folder = model.model_meta.name.clone();
     const SRC_FOLDER: &str = "src";
     if Path::new(&bevy_folder).exists() {
@@ -307,14 +383,36 @@ fn write_to_file(mut model: BevyModel) -> std::io::Result<()> {
         fs::create_dir(bevy_folder.to_owned())?;
     }
     fs::create_dir(bevy_folder.to_owned() + "/" + &SRC_FOLDER.to_owned())?;
+
+    //Write plugin or main/game
+    let bevy_type_filename = match model.model_meta.bevy_type {
+        BevyType::App => "/main.rs",
+        _ => "/lib.rs",
+    };
     let mut bevy_lib_file =
-        File::create(bevy_folder.to_owned() + "/" + &SRC_FOLDER.to_owned() + "/main.rs")?;
+        File::create(bevy_folder.to_owned() + "/" + &SRC_FOLDER.to_owned() + bevy_type_filename)?;
     let _ = bevy_lib_file
         .write("#![cfg_attr(not(debug_assertions), windows_subsystem = \"windows\")]".as_bytes());
     bevy_lib_file.write_all(model.generate().to_string().as_bytes())?;
 
+    //Write examples
+    if !model.examples.is_empty() {
+        fs::create_dir(bevy_folder.to_owned() + "/" + "examples")?;
+        for example in model.examples {
+            let mut bevy_example_file = File::create(
+                bevy_folder.to_owned() + "/examples/" + &example.model_meta.name + ".rs",
+            )?;
+            bevy_example_file.write_all(example.generate().to_string().as_bytes())?;
+        }
+    }
+
+    //Write cargo toml
     let mut cargo_file = File::create(bevy_folder.to_owned() + "/Cargo.toml")?;
-    let mut buf = format!(
+
+    let features = feature_write(model.bevy_settings.features);
+    let dev_features = feature_write(model.bevy_settings.dev_features);
+
+    let buf = format!(
         r#"[package]
 name = "{meta_name}"
 version = "0.1.0"
@@ -338,23 +436,17 @@ winit = {{ version = "0.25", features=["x11"]}}
 
 [dependencies.bevy]
 version = "0.6"
-"#,
-        meta_name = bevy_folder
-    );
+{features}
 
-    if model.bevy_settings.features.is_empty() {
-        buf += "default-features = false";
-    } else {
-        buf += "features = [";
-        let len = model.bevy_settings.features.len();
-        for (i, feature) in model.bevy_settings.features.into_iter().enumerate() {
-            buf += format!("\"{}\"", feature.to_feature()).as_str();
-            if i != len - 1 {
-                buf += ",";
-            }
-        }
-        buf += "]";
-    }
+[dev-dependencies.bevy]
+version = "0.6"
+{dev_features}
+
+"#,
+        meta_name = bevy_folder,
+        features = features,
+        dev_features = dev_features,
+    );
 
     cargo_file.write_all(buf.as_bytes())?;
     Ok(())
